89c8701add19820fc1d5860715ce3f1f20d9dbd3
Francesco <francesco.argentieri89@gmail.com>
2018-11-01 18:04:32 +0100

    bug: not working with tensor 299 x 299

--- movidiusinterface.py
+++ movidiusinterface.py
@@ -5,26 +5,26 @@
 from mvnc import mvncapi
 import os
 import errno
+import numpy as np
 
+# suppress warning and error message tf
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 
 class Singleton(type):
     _instances = {}
+
     def __call__(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
 
 
-
-
-
-
 # main entry point for the program
 class MovidiusInterface(metaclass=Singleton):
     def __init__(self):
         # set the logging level for the NC API
-        mvncapi.global_set_option(mvncapi.GlobalOption.RW_LOG_LEVEL, mvncapi.LogLevel.WARN)
+        mvncapi.global_set_option(mvncapi.GlobalOption.RW_LOG_LEVEL, mvncapi.LogLevel.DEBUG)
         # get a list of names for all the devices plugged into the system
         device_list = mvncapi.enumerate_devices()
         if not device_list:
@@ -88,86 +88,39 @@ class GraphNeuralNetwork(MovidiusInterface):
                 raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filename)
 
         with open(filename, mode='rb') as f:
-            blob = f.read()
+            graph_buffer = f.read()
         # Load the graph buffer into the NCS
         self.graph = mvncapi.Graph(filename)
         # Set up fifos
-        self.__fifo_in, self.__fifo_out = self.graph.allocate_with_fifos(self._device, blob)
-
-    # ---- Step 3: Pre-process the images ----------------------------------------
-
-    # def pre_process_image():
-    #
-    #     # Read & resize image [Image size is defined during training]
-    #     img = skimage.io.imread( ARGS.image )
-    #     img = skimage.transform.resize( img, ARGS.dim, preserve_range=True )
-    #
-    #     # Convert RGB to BGR [skimage reads image in RGB, but Caffe uses BGR]
-    #     if( ARGS.colormode == "BGR" ):
-    #         img = img[:, :, ::-1]
-    #
-    #     # Mean subtraction & scaling [A common technique used to center the data]
-    #     img = ( img - ARGS.mean ) * ARGS.scale
-    #
-    #     return img
-
-    # ---- Step 4: Read & print inference results from the NCS -------------------
+        self.__fifo_in, self.__fifo_out = self.graph.allocate_with_fifos(self._device, graph_buffer)
 
     def predict(self, test_image):
         """
         Read & print inference results from the NCS
         :param test_image:
         """
-        # Load the labels file
-        #labels = [line.rstrip('\n') for line in open(ARGS.labels) if line != 'classes\n']
-
         # The first inference takes an additional ~20ms due to memory
         # initializations, so we make a 'dummy forward pass'.
-        self.graph.queue_inference_with_fifo_elem(self.__fifo_in,
-                                                  self.__fifo_out,
-                                                  test_image,
-                                                  None)
+        self.graph.queue_inference_with_fifo_elem(self.__fifo_in, self.__fifo_out, test_image, None)
         output, userobj = self.__fifo_out.read_elem()
-
         # Load the image as an array
-        self.graph.queue_inference_with_fifo_elem(self.__fifo_in,
-                                                  self.__fifo_out,
-                                                  test_image,
-                                                  None)
+        self.graph.queue_inference_with_fifo_elem(self.__fifo_in, self.__fifo_out, test_image, None)
         # Get the results from NCS
         output, userobj = self.__fifo_out.read_elem()
-        # Sort the indices of top predictions
-        #order = output.argsort()[::-1][:NUM_PREDICTIONS]
+
         # Get execution time
         inference_time = self.graph.get_option(mvncapi.GraphOption.RO_TIME_TAKEN)
-        print(inference_time)
-
-        # # Print the results
-        # print("\n==============================================================")
-        # print("Top predictions for", ntpath.basename(ARGS.image))
-        # print("Execution time: " + str(numpy.sum(inference_time)) + "ms")
-        # print("--------------------------------------------------------------")
-        # for i in range(0, NUM_PREDICTIONS):
-        #     print("%3.1f%%\t" % (100.0 * output[order[i]]) + labels[order[i]])
-        # print("==============================================================")
-        #
-        # # If a display is available, show the image on which inference was performed
-        # if 'DISPLAY' in os.environ:
-        #     skimage.io.imshow(ARGS.image)
-        # skimage.io.show()
+        print("Execution time: {:5.5f} ms".format(np.sum(inference_time)))
         return output
 
-    def __del__(self):
+    def __clean(self):
         """
         Close and clean up fifos, graph
         :param self:
         """
-        if self.__fifo_in is not None:
-            self.__fifo_in.destroy()
-        if self.__fifo_out is not None:
-            self.__fifo_out.destroy()
-        if self.graph is not None:
-            self.graph.destroy()
+        self.__fifo_in.destroy()
+        self.__fifo_out.destroy()
+        self.graph.destroy()
         # Close the device and destroy the device handle
         try:
             self._device.close()
@@ -176,6 +129,9 @@ class GraphNeuralNetwork(MovidiusInterface):
             print("NCS device working.")
         except:
             raise Exception("Error - could not close NCS device.")
+
+    def __del__(self):
+        self.__clean()
 
 
 if __name__ == '__main__':
