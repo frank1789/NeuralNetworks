80d7d8f417eff9b8adedebf8420415aeef0a0918
Francesco <francesco.argentieri89@gmail.com>
2018-11-07 10:14:47 +0100

    init report branch

--- utilityfunction/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lib import Spinner
-from .lib import PrepareDataset
-from .lib import DataSet
-from .lib import TestSet
-#from .lib import PreparePicture
--- utilityfunction/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-from .loader import Spinner
-from .preparedata import PrepareDataset
-from .preparedata import DataSet
-from .preparedata import TestSet
-#from .image import PreparePicture
